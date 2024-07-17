2f2474109864e3343102a88e8cf970687b8b76f8
volodymyr-hlavnyi <volodymyr.hlavnyi@gmail.com>
2024-07-17 16:11:40 +0200

    Update files

--- Volodymyr/make_volodymyr.sh
+++ Volodymyr/make_volodymyr.sh
@@ -15,7 +15,7 @@ NUM_FILES=10
 
 for ((i=1; i<=NUM_FILES; i++))
 do
-  FILE_NAME="$working_dir/${i}_$(date '+%d%m%y').txt"
+  FILE_NAME="$working_dir/${i}_$(date '+%d.%m.%y').txt"
   TEXT=$(head /dev/urandom | head -c 100)
   echo "$TEXT" > $FILE_NAME
   echo "Create $FILE_NAME"
